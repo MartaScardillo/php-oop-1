a79d03f3ebd76d08bbba7efa8b3a23b2a0108f31
Marta Scardillo <martascardillo02@gmail.com>
2023-11-28 16:23:48 +0100

    Add echo list

--- index.php
+++ index.php
@@ -14,17 +14,54 @@ class Production {
         return $this->title;
     }
 
-    public function setTitle($_title) {
-        $this->title = $_title;
+    public function getLanguage() {
+        return $this->language;
     }
+
+    public function getRating() {
+        return $this->rating;
+    }
+
+    // public function setTitle($_title) {
+    //     $this->title = $_title;
+    // }
+
+
 }
 
 $prod1 = new Production('The Good Doctor', 'English', '9/10');
 $prod2 = new Production('Clannad', 'Japanese', 'Pianto isterico/10');
-
-echo $prod1->getTitle();
-echo $prod1->language;
-
-echo $prod2->getTitle();
-echo $prod2->language;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div class="container">
+        <ul>
+            <h1>Productions</h1>
+            <li class="prod">
+                <ul>
+                    <span>Production 1</span>
+                    <li><?php echo 'Title: ' . $prod1->getTitle() ?></li>
+                    <li><?php echo 'Language: ' . $prod1->getLanguage() ?></li>
+                    <li><?php echo 'Rating: ' . $prod1->getRating() ?></li>
+                </ul>
+            </li>
+            <li class="prod">
+                <ul>
+                    <span>Production 2</span>
+                    <li><?php echo 'Title: ' . $prod2->title ?></li>
+                    <li><?php echo 'Language: ' . $prod2->language ?></li>
+                    <li><?php echo 'Rating: ' . $prod2->rating ?></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
